be4a8b547a43e7424c27fd9a31953d1704281310
Edi316 <61267443+Edi316@users.noreply.github.com>
2024-10-13 15:52:16 +0200

    Change Config to Higher Resolution & Details

--- Vocaluxe/Base/CConfig.cs
+++ Vocaluxe/Base/CConfig.cs
@@ -1,4 +1,4 @@
-﻿#region license
+#region license
 // This file is part of Vocaluxe.
 // 
 // Vocaluxe is free software: you can redistribute it and/or modify
@@ -79,16 +79,16 @@ namespace Vocaluxe.Base
         [DefaultValue(ERenderer.TR_CONFIG_OPENGL)] public ERenderer Renderer;
 #endif
 
-            [DefaultValue(ETextureQuality.TR_CONFIG_TEXTURE_MEDIUM)]
+            [DefaultValue(ETextureQuality.TR_CONFIG_TEXTURE_HIGHEST)]
             public ETextureQuality TextureQuality;
-            [XmlRanged(32, 1024), DefaultValue(256)]
+            [XmlRanged(32, 1024), DefaultValue(512)]
             public int CoverSize;
 
             [XmlRanged(1, 6), DefaultValue(1)]
             public int NumScreens;
-            [DefaultValue(1024)]
+            [DefaultValue(1920)]
             public int ScreenW;
-            [DefaultValue(576)]
+            [DefaultValue(1080)]
             public int ScreenH;
             [DefaultValue(EGeneralAlignment.Middle)]
             public EGeneralAlignment ScreenAlignment;
@@ -116,11 +116,11 @@ namespace Vocaluxe.Base
 
         public struct SConfigTheme
         {
-            [XmlElement("Name"), DefaultValue("Genius")] public string Theme;
+            [XmlElement("Name"), DefaultValue("Energy")] public string Theme;
             [DefaultValue("Standard")] public string Skin;
-            [XmlElement("Cover"), DefaultValue("Genius Standard")] public string CoverTheme;
+            [XmlElement("Cover"), DefaultValue("Energy")] public string CoverTheme;
             [DefaultValue(EOffOn.TR_CONFIG_ON)] public EOffOn DrawNoteLines;
-            [DefaultValue(EOffOn.TR_CONFIG_ON)] public EOffOn DrawToneHelper;
+            [DefaultValue(EOffOn.TR_CONFIG_OFF)] public EOffOn DrawToneHelper;
             [DefaultValue(ETimerLook.TR_CONFIG_TIMERLOOK_EXPANDED)] public ETimerLook TimerLook;
             [DefaultValue(EPlayerInfo.TR_CONFIG_PLAYERINFO_BOTH)] public EPlayerInfo PlayerInfo;
             [DefaultValue(EFadePlayerInfo.TR_CONFIG_FADEPLAYERINFO_OFF)] public EFadePlayerInfo FadePlayerInfo;
@@ -149,7 +149,7 @@ namespace Vocaluxe.Base
             public EOffOn BackgroundMusicUseStart;
             [XmlRanged(0, 100), DefaultValue(50)]
             public int PreviewMusicVolume;
-            [XmlRanged(0, 100), DefaultValue(80)]
+            [XmlRanged(0, 100), DefaultValue(95)]
             public int GameMusicVolume;
             [DefaultValue(EOffOn.TR_CONFIG_OFF)]
             public EOffOn KaraokeEffect;
@@ -164,27 +164,27 @@ namespace Vocaluxe.Base
             public string Language;
             public string[] SongFolder;
             // ReSharper disable MemberHidesStaticFromOuterClass
-            [DefaultValue(ESongMenu.TR_CONFIG_TILE_BOARD)]
+            [DefaultValue(ESongMenu.TR_CONFIG_LIST)]
             public ESongMenu SongMenu;
             // ReSharper restore MemberHidesStaticFromOuterClass
-            [DefaultValue(ESongSorting.TR_CONFIG_ARTIST)] public ESongSorting SongSorting;
+            [DefaultValue(ESongSorting.TR_CONFIG_ARTIST_LETTER)] public ESongSorting SongSorting;
             [DefaultValue(EOffOn.TR_CONFIG_ON)] public EOffOn IgnoreArticles;
             [DefaultValue(ETimerMode.TR_CONFIG_TIMERMODE_REMAINING)] public ETimerMode TimerMode;
             [XmlAltName("NumPlayer"), DefaultValue(2)] public int NumPlayers;
-            [DefaultValue(EOffOn.TR_CONFIG_OFF)] public EOffOn Tabs;
-            [DefaultValue(EOffOn.TR_CONFIG_OFF)] public EOffOn AutoplayPreviews;
+            [DefaultValue(EOffOn.TR_CONFIG_ON)] public EOffOn Tabs;
+            [DefaultValue(EOffOn.TR_CONFIG_ON)] public EOffOn AutoplayPreviews;
             [XmlAltName("AutoplayPreviewDelay"), DefaultValue(500)] public int AutoplayPreviewDelay;
             [DefaultValue(ELyricsPosition.TR_CONFIG_LYRICSPOSITION_BOTTOM)] public ELyricsPosition LyricsPosition;
             [DefaultValue(0.1f)] public float MinLineBreakTime; //Minimum time to show the text before it is (to be) sung (if possible)
             [XmlArrayItem("Player"), XmlArray] public string[] Players;
-            [DefaultValue(EHighscoreStyle.TR_CONFIG_HIGHSCORE_LIST_BEST)] public EHighscoreStyle HighscoreStyle;
+            [DefaultValue(EHighscoreStyle.TR_CONFIG_HIGHSCORE_LIST_ALL)] public EHighscoreStyle HighscoreStyle;
         }
 
         public struct SConfigVideo
         {
             [DefaultValue(EVideoDecoder.FFmpeg)]
             public EVideoDecoder VideoDecoder;
-            [DefaultValue(EOffOn.TR_CONFIG_ON)]
+            [DefaultValue(EOffOn.TR_CONFIG_OFF)]
             public EOffOn VideoBackgrounds;
             [DefaultValue(EOffOn.TR_CONFIG_ON)]
             public EOffOn VideoPreview;
@@ -200,7 +200,7 @@ namespace Vocaluxe.Base
         public struct SConfigRecord
         {
             public SMicConfig[] MicConfig;
-            [DefaultValue(200)]
+            [DefaultValue(180)]
             public int MicDelay; //[ms]
         }
 
@@ -440,7 +440,7 @@ namespace Vocaluxe.Base
                 case "TextureQuality":
                     return "TextureQuality: " + CHelper.ListStrings(Enum.GetNames(typeof(ETextureQuality)));
                 case "CoverSize":
-                    return "CoverSize (pixels): 32, 64, 128, 256, 512, 1024 (default: 128)";
+                    return "CoverSize (pixels): 32, 64, 128, 256, 512, 1024 (default: 512)";
                 case "NumScreens":
                     return "Number of screens to use. 1 - 6 (default: 1)";
                 case "ScreenW":
